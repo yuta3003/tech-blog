179ae0432b8dbb1acdcc83ea054f9bfa18643ede
yuta3003 <z-nakazawa@srapc02686>
2023-04-28 13:20:53 +0900

    footer追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,12 +78,91 @@ const Home: NextPage<Props> = ({ allPosts }) => {
           ))}
         </div>
       </main>
+
       <footer className="
-        px-5
-        py-3"
+        bg-white
+        rounded-lg
+        shadow
+        m-4
+        dark:bg-gray-800"
       >
-        footer
+        <div className="
+          w-full
+          mx-auto
+          max-w-screen-xl
+          p-4
+          md:flex
+          md:items-center
+          md:justify-between"
+        >
+          <span className="
+            text-sm
+            text-gray-500
+            sm:text-center
+            dark:text-gray-400"
+          >
+            © 2023
+          <a href="https://flowbite.com/"
+            className="hover:underline"
+          >
+            Flowbite™
+          </a>. All Rights Reserved.
+          </span>
+          <ul className="
+            flex
+            flex-wrap
+            items-center
+            mt-3
+            text-sm
+            font-medium
+            text-gray-500
+            dark:text-gray-400
+            sm:mt-0"
+          >
+            <li>
+              <a href="#"
+                className="
+                  mr-4
+                  hover:underline
+                  md:mr-6"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                className="
+                  mr-4
+                  hover:underline
+                  md:mr-6"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                className="
+                  mr-4
+                  hover:underline
+                  md:mr-6"
+              >
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#"
+                className="
+                  mr-4
+                  hover:underline
+                  md:mr-6"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </footer>
+
     </div>
   );
 };
